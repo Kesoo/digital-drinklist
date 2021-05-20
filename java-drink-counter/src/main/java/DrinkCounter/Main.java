b8e58743a6353bb416567614508eaa3f8ae17b73
Anton Ericson <antonericson97@gmail.com>
2021-05-20 21:19:15 +0200

    Added Maven to Java project

--- java-drink-counter/src/main/java/DrinkCounter/Main.java
+++ java-drink-counter/src/main/java/DrinkCounter/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("openFile.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/openFile.fxml"));
         primaryStage.setTitle("RUSTET Drink Counter");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
